5107c886d7df3f598cb865af1fc3b1b73e9628df
JOHN CARL EDCEL A MANUEL <edcelmanuel9@gmail.com>
2021-08-18 20:36:53 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import React, { useState } from "react"
+import { Document, Page, pdfjs } from "react-pdf"
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
 function App() {
-    return <div className="App"></div>
+    const [numPages, setNumPages] = useState(null)
+    const [pageNumber, setPageNumber] = useState(2)
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages)
+    }
+    return (
+        <div className="flex items-center content-center justify-center w-screen h-screen bg-red-400">
+            <Document file="http://localhost:3220/pdf/text.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
+            </Document>
+            <p>
+                Page {pageNumber} of {numPages}
+            </p>
+        </div>
+    )
 }
 
 export default App
